79491e56012c2233d95fb507eca0aa27ac82aa5c
macnick <nick@macexperts.gr>
2020-03-04 20:19:15 +0200

    Allow players to add their mark to the DOM

--- assets/index.js
+++ assets/index.js
@@ -1,27 +1,11 @@
-import Player from "/player.js";
-
-const model = (() => {
-  const Table = () => {
-    const t = [null, null, null, null, null, null, null, null, null];
-    return { t };
-  };
-
-  // const Player = (name, marker) => {
-  //   const getName = () => name;
-  //   const getMarker = () => marker;
-  //   return { getName, getMarker };
-  // };
-  return { Table };
-})();
+import Player from "./player.js";
+import Table from "./board.js";
 
 const view = (() => {
   const displayBoard = board => {
     let boxes = Array.from(document.getElementsByClassName("box"));
     boxes.forEach((box, i) => {
       box.innerHTML = board.t[i];
-      if (box.innerHTML == "") {
-        box.addEventListener("click", controller.putSymbol(i));
-      }
     });
   };
   // inputPlayers = () => {};
@@ -29,19 +13,34 @@ const view = (() => {
   return { displayBoard };
 })();
 
-const controller = ((model, view) => {
-  let t = model.Table(),
+const controller = (view => {
+  let t = Table(),
     p1 = Player("Name1", "X"),
     p2 = Player("Anonymous", "0"),
     currentPlayer = p1;
-  const putSymbol = i => {
-    let box = document.getElementById(i);
-    box.innerHTML = currentPlayer.getMarker();
-    // change player
-    currentPlayer = currentPlayer == p1 ? p2 : p1;
+
+  const addListeners = t => {
+    let boxes = Array.from(document.getElementsByClassName("box"));
+    boxes.forEach((box, i) => {
+      box.addEventListener("click", putSymbol, false);
+    });
   };
+
+  const putSymbol = e => {
+    console.log(e.target.innerHTML);
+    if (e.target.innerHTML == "") {
+      t.t[e.target.id] = currentPlayer.getMarker();
+      // change player
+      currentPlayer = currentPlayer == p1 ? p2 : p1;
+      view.displayBoard(t);
+    }
+  };
+
   // t.t[4] = "X";
   // t.t[2] = "O";
+  // t.t[0] = "X";
+  // console.log(t.t);
+  addListeners(t);
   view.displayBoard(t);
 
   return { t, currentPlayer };
@@ -52,4 +51,4 @@ const controller = ((model, view) => {
   // checkWinner;
 
   // checkEndGame;
-})(model, view);
+})(view);
